124e370af5631264dd619dc91cd60af012e38739
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2026-06-18 19:44:48 -0300

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for my i386 bootloader";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nasm
+            qemu
+            git
+          ];
+        };
+      }
+    ) // {
+      templates.default = {
+        path = ./.;
+        description = "from my github template";
+      };
+    };
+}
